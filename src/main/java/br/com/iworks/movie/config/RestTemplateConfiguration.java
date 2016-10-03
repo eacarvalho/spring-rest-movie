49e45eddf96651713a3c6c3d82c26c02e5a57870
Eduardo Alvim de Carvalho <ext.eduardo.carvalho@netshoes.com>
2016-10-03 11:58:23 -0300

    Configuracao de proxy no resttemplate

--- src/main/java/br/com/iworks/movie/config/RestTemplateConfiguration.java
+++ src/main/java/br/com/iworks/movie/config/RestTemplateConfiguration.java
@@ -1,8 +1,15 @@
 package br.com.iworks.movie.config;
 
+import static org.apache.http.client.config.RequestConfig.Builder;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,21 +22,55 @@ public class RestTemplateConfiguration {
 
     private final static int TIMEOUT = 5000;
 
+    @Value("${proxy.enabled}")
+    private boolean isProxyEnabled;
+
+    @Value("${proxy.host}")
+    private String proxyHost;
+
+    @Value("${proxy.port}")
+    private Integer proxyPort;
+
+    @Value("${proxy.username}")
+    private String proxyUsername;
+
+    @Value("${proxy.password}")
+    private String proxyPassword;
+
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.setConnectTimeout(TIMEOUT).setReadTimeout(TIMEOUT).build();
+    public RestTemplate restTemplate(RestTemplateBuilder builder, ClientHttpRequestFactory clientHttpRequestFactory) {
+        return builder.requestFactory(clientHttpRequestFactory).build();
     }
 
     @Bean
     public ClientHttpRequestFactory clientHttpRequestFactory() {
-        RequestConfig config = RequestConfig.custom()
+        Builder requestConfigBuilder = RequestConfig.custom()
                 .setConnectTimeout(TIMEOUT)
                 .setConnectionRequestTimeout(TIMEOUT)
-                .setSocketTimeout(TIMEOUT)
-                .build();
+                .setSocketTimeout(TIMEOUT);
 
-        CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+        HttpClientBuilder httpClientBuilder = this.getHttpClientBuilder(requestConfigBuilder);
 
-        return new HttpComponentsClientHttpRequestFactory(client);
+        return new HttpComponentsClientHttpRequestFactory(httpClientBuilder.build());
+    }
+
+    private HttpClientBuilder getHttpClientBuilder(final Builder requestConfigBuilder) {
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+
+        if (isProxyEnabled) {
+            CredentialsProvider credentialProvider = getCredentialsProvider(requestConfigBuilder);
+            httpClientBuilder.setDefaultCredentialsProvider(credentialProvider).setDefaultRequestConfig(requestConfigBuilder.build());
+        } else {
+            httpClientBuilder.setDefaultRequestConfig(requestConfigBuilder.build());
+        }
+
+        return httpClientBuilder;
+    }
+
+    private CredentialsProvider getCredentialsProvider(final Builder requestConfigBuilder) {
+        requestConfigBuilder.setProxy(new HttpHost(proxyHost, proxyPort));
+        CredentialsProvider credentialProvider = new BasicCredentialsProvider();
+        credentialProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(proxyUsername, proxyPassword));
+        return credentialProvider;
     }
 }
