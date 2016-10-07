ab7864856dfa530be57788a897f66a4a9c1e65b5
Eduardo Alvim de Carvalho <ext.eduardo.carvalho@netshoes.com>
2016-10-07 11:59:17 -0300

    Configuração de Cors

--- src/main/java/br/com/iworks/movie/config/CorsConfiguration.java
+++ src/main/java/br/com/iworks/movie/config/CorsConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -23,6 +24,15 @@ public class CorsConfiguration extends WebMvcConfigurerAdapter {
                         HttpMethod.OPTIONS.name(),
                         HttpMethod.HEAD.name(),
                         HttpMethod.TRACE.name())
+                .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
