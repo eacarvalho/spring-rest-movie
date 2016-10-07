1bd42dbe569b31003cc8c31e44da98251316f625
Eduardo Alvim de Carvalho <ext.eduardo.carvalho@netshoes.com>
2016-10-07 10:31:09 -0300

    Configuração do Cors

--- src/main/java/br/com/iworks/movie/config/CorsConfiguration.java
+++ src/main/java/br/com/iworks/movie/config/CorsConfiguration.java
@@ -3,17 +3,18 @@ package br.com.iworks.movie.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("*")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .allowedMethods(HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
                         HttpMethod.GET.name(),
@@ -21,6 +22,7 @@ public class CorsConfiguration extends WebMvcConfigurerAdapter {
                         HttpMethod.PATCH.name(),
                         HttpMethod.OPTIONS.name(),
                         HttpMethod.HEAD.name(),
-                        HttpMethod.TRACE.name());
+                        HttpMethod.TRACE.name())
+                .maxAge(3600);
     }
 }
