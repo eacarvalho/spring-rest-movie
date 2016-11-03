5f26b3c0b8fa96b8ae97fca501bf3e5ec8bb4656
Eduardo Alvim de Carvalho <ext.eduardo.carvalho@netshoes.com>
2016-11-03 18:15:35 -0200

    Configuracao inicial de oauth2 token

--- src/main/java/br/com/iworks/movie/config/Swagger2Configuration.java
+++ src/main/java/br/com/iworks/movie/config/Swagger2Configuration.java
@@ -1,10 +1,16 @@
 package br.com.iworks.movie.config;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
@@ -12,12 +18,9 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -44,6 +47,30 @@ public class Swagger2Configuration {
                         // .paths(PathSelectors.ant("v1/*"))
                 .build();
     }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return new SecurityConfiguration(
+                null, null, null,
+                "movies",
+                "Bearer",
+                ApiKeyVehicle.HEADER,
+                "Authorization",
+                null);
+    }
+
+    /*
+    @Bean
+    public SecurityConfiguration security() {
+        return new springfox.documentation.swagger.web.SecurityConfiguration(
+                null, null, null,
+                "myapp-api",
+                "access_token",
+                ApiKeyVehicle.QUERY_PARAM,
+                "access_token",
+                null);
+    }
+    */
 
     // http://www.restapitutorial.com/lessons/httpmethods.html
     private ArrayList<ResponseMessage> getPostResponseMessages() {
